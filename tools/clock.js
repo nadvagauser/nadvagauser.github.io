e5c188dca13c2d0c1b5cb1a3577d1547c3800bcc
nadvagauser <83984667+nadvagauser@users.noreply.github.com>
2022-05-31 05:32:59 +0200

    Add a hour-of-the-year clock

---
+++ tools/clock.js
@@ -0,0 +1,39 @@
+const clockTime =
+  document.querySelector ('#clock-time')
+
+const clockProgress =
+  document.querySelector ('#clock-progress')
+
+function update() {
+  const now =
+    Temporal.Now.zonedDateTimeISO ()
+
+  const hourOfYear =
+    (now.dayOfYear - 1) * now.hoursInDay + now.hour
+
+  const hour =
+    String (hourOfYear)
+    . padStart (4, '0')
+  
+  const minute =
+    String (now.minute)
+    . padStart (2, '0')
+  
+  const second =
+    String (now.second)
+    . padStart(2, '0')
+
+  // Formatted output should look like 0000:00:00
+  clockTime.textContent =
+    `${hour}:${minute}:${second}`
+  
+  // Needs to be constantly refreshed in case of a year or timezone change
+  clockProgress.max =
+    now.daysInYear * now.hoursInDay
+  
+  clockProgress.value =
+    hourOfYear
+}
+
+update ()
+setInterval (update, 1000)
